08a930bb8f1e5001134b2b0ce2128904dec4fb3f
金正朋也 <mhtnim1023@gmail.com>
2023-02-19 18:47:23 +0900

    Feat: b treeの雛形作成

--- rucene-core/src/models/inverted_index.rs
+++ rucene-core/src/models/inverted_index.rs
@@ -4,10 +4,9 @@ use crate::models::term_list::TermList;
 // ハッシュマップでドキュメントとポスティングリストのマッピングを実現している
 // [重要] 本ではタームリスト自体はRDBで管理している。なので自前でタームリストのb+treeデータ構造を作成・持つ必要はないが
 // 今回は深堀りのためにタームリストもRDB使ってディスクで管理するのではなく、メモリ上でb+treeで持たせる！
-pub struct InvertedIndex<'a> {
-    term_list: TermList
+pub struct InvertedIndex {
+    term_list: TermList,
 }
-
 
 pub struct PostingsListWrapper {
     document_count: i32, // 対象単語が出現するドキュメントの総数（linked listで紐付くPostingsList自体の総数）
--- rucene-core/src/models/term_list.rs
+++ rucene-core/src/models/term_list.rs
@@ -1,13 +1,70 @@
 use crate::models::inverted_index::PostingsListWrapper;
+use std::collections::HashMap;
+// btreeで実装（完成後、b+treeでも作ってみる）
+// 参考：https://www.javatips.net/api/algs4-master/src/main/java/edu/princeton/cs/algs4/BTree.java
 
-// TODO: b+treeで実装
+// 一旦orderは4で作成してみる
+// 本来ならディスクのブロックサイズを考慮したorder数にすべきだがまたの機会に調整
+static ORDER: usize = 4;
+
 pub struct TermList {
-    root: Node
+    root: Node,
+    height: i32,
+}
+
+impl TermList {
+    fn init() -> TermList {
+        TermList {
+            root: Node::new(),
+            height: 0,
+        }
+    }
+
+    fn insert(&mut self, term_list: HashMap<String, PostingsListWrapper>) -> TermList {
+        // self.root.insert()
+        todo!()
+    }
+}
+
+// key value childrenが存在しないEntryを作成することもあるので、Optionで定義
+pub struct Entry {
+    key: Option<String>,
+    value: Option<PostingsListWrapper>,
+    next: Option<Box<Node>>,
+}
+
+impl Entry {
+    fn new(
+        key: Option<String>,
+        value: Option<PostingsListWrapper>,
+        next: Option<Box<Node>>,
+    ) -> Entry {
+        Entry { key, value, next }
+    }
 }
 
 pub struct Node {
-    term: String,
-    postings_list: PostingsListWrapper,
-    next: Box<Node>,
-    previous: Box<Node>
+    num_of_children: i32,
+    children: Vec<Entry>,
+}
+
+impl Node {
+    fn new() -> Node {
+        Node {
+            num_of_children: 0,
+            children: Vec::with_capacity(ORDER),
+        }
+    }
+
+    fn insert(&mut self, key: String, value: PostingsListWrapper) {
+        let entry = Entry::new(Some(key), Some(value), None);
+        if self.num_of_children == 0 {
+            self.children.push(entry)
+        } else {
+            // TODO:
+            //  for文回してkeyの順序を計算しながらleaf nodeまで深く潜る
+
+            self.children.iter_mut().for_each(|a| ());
+        }
+    }
 }
